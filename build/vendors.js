dc6f2c3e1f006703e14dfad30b106b9200e21f48
Maksim timonov <m@timonov.me>
2020-05-03 19:18:36 +0300

    update

--- build/vendors.js
+++ build/vendors.js
@@ -25008,6 +25008,31 @@ exports.DeviceSkin = DeviceSkin;
 //# sourceMappingURL=index.js.map
 
 
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone8-space-gray/build/index.js":
+/*!***********************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone8-space-gray/build/index.js ***!
+  \***********************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 871;
+        this.imageHeight = 1776;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
 /***/ })
 
 }]);
