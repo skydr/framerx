c69e0f017480f8be04ab612a5bc172e946d03797
Maksim timonov <m@timonov.me>
2020-05-03 13:19:57 +0300

    update

--- build/vendors.js
+++ build/vendors.js
@@ -43,6 +43,31 @@ exports.DeviceSkin = DeviceSkin;
 
 /***/ }),
 
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-coral/build/index.js":
+/*!********************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-coral/build/index.js ***!
+  \********************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1074;
+        this.imageHeight = 2038;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
 /***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-red/build/index.js":
 /*!******************************************************************************************************************************************************************************************************************************!*\
   !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xr-red/build/index.js ***!
@@ -83,6 +108,31 @@ class DeviceSkin {
         this.image = "device.png";
         this.imageWidth = 1074;
         this.imageHeight = 2038;
+        this.padding = 40;
+        this.background = "black";
+    }
+}
+exports.DeviceSkin = DeviceSkin;
+//# sourceMappingURL=index.js.map
+
+
+/***/ }),
+
+/***/ "framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xs-spacegrey/build/index.js":
+/*!************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Applications/Framer X.app/Contents/Resources/Server/node_modules/@framerjs/build-library/build/Compiler/loaders/framer-package-loader.js!./node_modules/@framer/framer.device-skin-apple-iphone-xs-spacegrey/build/index.js ***!
+  \************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+class DeviceSkin {
+    constructor() {
+        this.image = "device.png";
+        this.imageWidth = 1385;
+        this.imageHeight = 2696;
         this.padding = 40;
         this.background = "black";
     }
